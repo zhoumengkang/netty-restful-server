a182a1fcc3d8f573756d73ce06be240c8f2b6744
zhoumengkang <i@zhoumengkang.com>
2016-01-05 19:40:48 +0800

    go home and go on

--- src/main/java/net/mengkang/api/response/ResponseHandler.java
+++ src/main/java/net/mengkang/api/response/ResponseHandler.java
@@ -1,10 +1,16 @@
 package net.mengkang.api.response;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.HashMap;
 import java.util.Map;
 
 
 public class ResponseHandler {
+
+    private static final Logger logger = LoggerFactory.getLogger(ResponseHandler.class);
+
     private static final Map<Integer, String> codeMap = new HashMap<>();
 
     public static final int UNKNOWN_ERROR         = 1000;
