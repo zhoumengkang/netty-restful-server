291fee81a875ef50c57fc0ce28567fc506197cd0
zhoumengkang <i@zhoumengkang.com>
2016-01-19 18:21:30 +0800

    MessageDigestUtil

---
+++ src/main/java/net/mengkang/nettyrest/utils/MessageDigestUtil.java
@@ -0,0 +1,28 @@
+package net.mengkang.nettyrest.utils;
+
+import java.security.MessageDigest;
+
+public class MessageDigestUtil {
+
+    public static String MD5(String s) {
+        char hexDigits[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+        try {
+            byte[] btInput = s.getBytes();
+            MessageDigest mdInst = MessageDigest.getInstance("MD5");
+            mdInst.update(btInput);
+            byte[] md = mdInst.digest();
+            int j = md.length;
+            char str[] = new char[j * 2];
+            int k = 0;
+            for (int i = 0; i < j; i++) {
+                byte byte0 = md[i];
+                str[k++] = hexDigits[byte0 >>> 4 & 0xf];
+                str[k++] = hexDigits[byte0 & 0xf];
+            }
+            return new String(str);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
