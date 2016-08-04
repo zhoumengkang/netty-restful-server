60dbe55d092cca4acd83850dd48e910ccf02f262
mengkang.zmk <mengkang.zmk@alibaba-inc.com>
2016-08-04 11:04:19 +0800

    业务完善

--- src/main/java/net/mengkang/demo/service/IconService.java
+++ src/main/java/net/mengkang/demo/service/IconService.java
@@ -2,12 +2,16 @@ package net.mengkang.demo.service;
 
 import net.mengkang.demo.bo.Icon;
 import net.mengkang.nettyrest.ApiProtocol;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 
 public class IconService extends BaseService {
+
+    private static final Logger logger = LoggerFactory.getLogger(IconService.class);
 
     public IconService(ApiProtocol apiProtocol) {
         super(apiProtocol);
@@ -22,7 +26,7 @@ public class IconService extends BaseService {
             try {
                 throw new Exception("illegal icon");
             } catch (Exception e) {
-                e.printStackTrace();
+                logger.error(e.getMessage());
             }
             return icon;
         }
